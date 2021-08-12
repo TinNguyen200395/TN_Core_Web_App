755f9a71e6b87ae1a58ef0fd19a5a83647f59c6d
tinnguyen200395 <tinnguyen33886@gmail.com>
2021-08-12 21:25:06 +0700

    Section21: Assign permissions to the roles

--- TN_Core_Web_App.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ TN_Core_Web_App.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -26,6 +26,9 @@ namespace TN_Core_Web_App.Services.AutoMapper
             CreateMap<AppUserViewModel, AppUser>()
            .ConstructUsing(c => new AppUser(c.Id.GetValueOrDefault(Guid.Empty), c.FullName, c.UserName,
            c.Email, c.PhoneNumber, c.Avatar, c.Status));
+
+            CreateMap<PermissionViewModel, Permission>()
+            .ConstructUsing(c => new Permission(c.RoleId, c.FunctionId, c.CanCreate, c.CanRead, c.CanUpdate, c.CanDelete));
         }
     }
 }
--- TN_Core_Web_App.Services/Interfaces/IFunctionService.cs
+++ TN_Core_Web_App.Services/Interfaces/IFunctionService.cs
@@ -9,8 +9,24 @@ namespace TN_Core_Web_App.Services.Interfaces
 {
     public interface IFunctionService : IDisposable
     {
-        Task<List<FunctionViewModel>> GetAll();
+        void Add(FunctionViewModel function);
 
-        List<FunctionViewModel> GetAllByPermission(Guid userId);
+        Task<List<FunctionViewModel>> GetAll(string filter);
+
+        IEnumerable<FunctionViewModel> GetAllWithParentId(string parentId);
+
+        FunctionViewModel GetById(string id);
+
+        void Update(FunctionViewModel function);
+
+        void Delete(string id);
+
+        void Save();
+
+        bool CheckExistedId(string id);
+
+        void UpdateParentId(string sourceId, string targetId, Dictionary<string, int> items);
+
+        void ReOrder(string sourceId, string targetId);
     }
 }

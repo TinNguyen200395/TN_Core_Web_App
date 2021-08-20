2b7951093cfd9b6dc33e619ad937003c17fc744b
tinnguyen200395 <tinnguyen33886@gmail.com>
2021-08-20 21:00:18 +0700

    Section 32 : Create the product category page

--- TN_Core_Web_App/Controllers/ProductController.cs
+++ TN_Core_Web_App/Controllers/ProductController.cs
@@ -1,24 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TN_Core_Web_App.Models.ProductViewModels;
+using TN_Core_Web_App.Services.Interfaces;
 
 namespace TN_Core_Web_App.Controllers
 {
-    public class ProductController :Controller
+    public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        IConfiguration _configuration;
+        public ProductController(IProductService productService, IConfiguration configuration,
+            IProductCategoryService productCategoryService)
+        {
+            _productService = productService;
+            _productCategoryService = productCategoryService;
+            _configuration = configuration;
+        }
         [Route("products.html")]
-
         public IActionResult Index()
         {
             return View();
         }
-        [Route("{alias}-c.{id}.html")]
 
-        public IActionResult Catalog(int id , string keyword,int? pageSize , string sortBy, int page=1)
+        [Route("{alias}-c.{id}.html")]
+        public IActionResult Catalog(int id, int? pageSize, string sortBy, int page = 1)
+        {
+            var catalog = new CatalogViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(id, string.Empty, page, pageSize.Value);
+            catalog.Category = _productCategoryService.GetById(id);
+
+            return View(catalog);
+        }
+
+        [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
+        public IActionResult Details(int id)
         {
             return View();
         }
+
     }
 }
